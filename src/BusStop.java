4764c7211a9f681b255bafbaa23e596c8a06d6df
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-10-18 12:45:03 +0530

    Add bus arrives logic

--- src/BusStop.java
+++ src/BusStop.java
@@ -6,4 +6,20 @@ public class BusStop {
     private Semaphore busMutex = new Semaphore(1);
     private Semaphore allAboard = new Semaphore(0);
     private int waitingRiderCount = 0;
+
+    public void busArrives() throws InterruptedException {
+        busMutex.acquire();
+        int boarding = Math.min(waitingRiderCount, BUS_CAPACITY);
+        System.out.println("Bus arrived. Boarding " + boarding + " riders");
+
+        for (int i = 0; i < boarding; i++) {
+            waitingRiders.release();
+        }
+
+        for (int i = 0; i < boarding; i++) {
+            allAboard.acquire();
+        }
+
+        waitingRiderCount -= boarding;
+    }
 }
