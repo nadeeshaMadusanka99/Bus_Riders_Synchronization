d16753077db750b79159d8b347a3eac64cdfef70
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-10-18 12:24:44 +0530

    Add initial classes set up

---
+++ src/BusSimulation.java
@@ -0,0 +1,10 @@
+public class BusSimulation {
+    public static void main(String[] args) {
+        // Create a new Bus object
+        Bus bus = new Bus();
+        // Create a new Rider object
+        Rider rider = new Rider();
+        // Create a new BusStop object
+        BusStop busStop = new BusStop();
+    }
+}
