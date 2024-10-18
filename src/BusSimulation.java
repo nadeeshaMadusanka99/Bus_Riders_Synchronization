f3c3e37791c11a7fac2e1ee05bcf900bbb69dec2
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-10-18 12:50:45 +0530

    Add bus arrival shedule logic

--- src/BusSimulation.java
+++ src/BusSimulation.java
@@ -1,5 +1,6 @@
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
 public class BusSimulation {
@@ -11,5 +12,21 @@ public class BusSimulation {
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(100);
         Random random = new Random();
 
+        // Schedule bus arrivals with exponential distribution
+        scheduleBusArrivals(executorService, busStop, random);
+
     }
+
+    private static void scheduleBusArrivals(ScheduledExecutorService executorService, BusStop busStop, Random random) {
+        Runnable busScheduler = new Runnable() {
+            @Override
+            public void run() {
+                executorService.execute(new Bus(busStop));
+                long delay = (long) (Math.log(1 - random.nextDouble()) / (-1.0 / BUS_MEAN_ARRIVAL_TIME) * 60 * 1000);
+                executorService.schedule(this, delay, TimeUnit.MILLISECONDS);
+            }
+        };
+        executorService.submit(busScheduler);
+    }
+
 }
