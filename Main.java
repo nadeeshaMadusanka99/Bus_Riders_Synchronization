cef87ae773475e64d73a95da77b1820aee58bd2e
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-10-18 10:18:53 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld.main(args);
+    }
+}
