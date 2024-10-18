eb220fba27f6aa48c29eef593a78c12099d4408c
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-10-18 12:52:42 +0530

    Add rider configuration

--- src/Rider.java
+++ src/Rider.java
@@ -1,3 +1,21 @@
-public class Rider {
+public class Rider implements Runnable {
+    private static int riderCount = 0;
+    private int id;
+    private BusStop busStop;
 
+    public Rider(BusStop busStop) {
+        this.busStop = busStop;
+        this.id = ++riderCount;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Rider " + id + " arrives at bus stop");
+
+            System.out.println("Rider " + id + " boards the bus");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
