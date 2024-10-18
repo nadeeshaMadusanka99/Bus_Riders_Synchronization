d074369c03466e4330792895244cd40c97713a3e
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-10-18 12:48:03 +0530

    Add bus class configuration

--- src/Bus.java
+++ src/Bus.java
@@ -1,3 +1,23 @@
-public class Bus {
+import java.util.concurrent.TimeUnit;
 
+public class Bus implements Runnable {
+    private BusStop busStop;
+
+    public Bus(BusStop busStop) {
+        this.busStop = busStop;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Bus is arriving");
+            busStop.busArrives();
+            // custom boarding time
+            TimeUnit.MILLISECONDS.sleep(500);
+            busStop.busDeparts();
+            System.out.println("Bus is departing");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
